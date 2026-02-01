a69b0a0bbddb744656afeb4f4fe4d35a6a680e17
a_yordanovv <aleksandar_yordanov2001@abv.bg>
2026-02-01 12:53:08 +0200

    Adding Location class

---
+++ logistics-app/src/logisticsapp/models/Location.java
@@ -0,0 +1,16 @@
+package logisticsapp.models;
+
+import java.time.LocalTime;
+
+public class Location {
+
+    private LocalTime departureTime;
+    private LocalTime expectedArrivalTIme;
+
+    public Location(LocalTime departureTime, LocalTime expectedArrivalTIme) {
+        this.departureTime = departureTime;
+        this.expectedArrivalTIme = expectedArrivalTIme;
+    }
+
+
+}
