fb10c6f0245621bc5abbc385d05204ea195ba2f1
a_yordanovv <aleksandar_yordanov2001@abv.bg>
2026-02-06 20:51:15 +0200

    Adding new commandConstans messages in CommandConstants class

--- logistics-app/src/logisticsapp/commands/CommandsConstants.java
+++ logistics-app/src/logisticsapp/commands/CommandsConstants.java
@@ -3,10 +3,15 @@ package logisticsapp.commands;
 public class CommandsConstants {
 
 
-    public static final String DELIVERY_PACKAGE_CREATED_MESSAGE = "Delivery package with ID %d was created";
+    public static final String DELIVERY_PACKAGE_CREATED_MESSAGE = "Delivery package with ID %d was created.";
 
     public static final String CUSTOMER_CREATED_MESSAGE = "Customer with ID %d was created.";
 
     public static final String TRUCK_CREATED_MESSAGE = "Truck with brand %s and Id %d was created.";
+
+    public static final String LOCATION_CREATED_MESSAGE = "Location with name %s was created.";
+
+    public static final String DELIVERY_ROUTE_CREATED_MESSAGE = "DeliveryRoute with ID %d, " +
+            "startlocation: %s and endlocation: %s was created.";
 
 }
