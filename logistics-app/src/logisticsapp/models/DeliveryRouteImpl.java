533ec7bcee136a4bf205af45f8851210249439ac
a_yordanovv <aleksandar_yordanov2001@abv.bg>
2026-02-06 20:54:25 +0200

    Fixing some things in DeliveryRouteImpl class

--- logistics-app/src/logisticsapp/models/DeliveryRouteImpl.java
+++ logistics-app/src/logisticsapp/models/DeliveryRouteImpl.java
@@ -1,26 +1,55 @@
 package logisticsapp.models;
 
-import logisticsapp.models.contracts.DeliveryPackage;
+import logisticsapp.exceptions.InvalidInput;
 import logisticsapp.models.contracts.DeliveryRoute;
+import logisticsapp.utils.ValidationHelpers;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DeliveryRouteImpl implements DeliveryRoute {
 
+    private static final String INVALID_LOCATIONS_COUNT_MESSAGE = "Locations can't be less than 2";
+
     private int id;
-    private List<DeliveryPackage> deliveryPackages;
+    private List<Location> locations;
+    private LocalDateTime departureTime;
+
     //locations to be implemented
 
-    public DeliveryRouteImpl(int id) {
+    public DeliveryRouteImpl(int id, List<Location> locations) {
         setId(id);
-        deliveryPackages = new ArrayList<>();
+        this.locations = new ArrayList<>();
+        setLocations(locations);
     }
 
+    public void startRoute() {
+        this.locations.get(0).setExpectedArrivalTime();
+//        System.out.println(locations.get(0).getFormattedExpectedArrivalTime());
+
+        for (int i = 1; i < locations.size(); i++) {
+            int distance = Distances.getDistance(locations.get(i - 1).getCity(), locations.get(i).getCity());
+            int travelTime = distance / 87;
+            locations.get(i)
+                    .setExpectedArrivalTime(locations.get(i - 1).getExpectedArrivalTime().plusHours(travelTime));
+
+//            System.out.println(locations.get(i).getFormattedExpectedArrivalTime());
+        }
+    }
 
     private void setId(int id) {
         this.id = id;
     }
+
+    public void setLocations(List<Location> deliveryLocations) {
+        if(deliveryLocations.size() < 2){
+            throw new InvalidInput(INVALID_LOCATIONS_COUNT_MESSAGE);
+        }
+
+        locations.addAll(deliveryLocations);
+    }
+
 
     @Override
     public int getID() {
@@ -33,15 +62,10 @@ public class DeliveryRouteImpl implements DeliveryRoute {
         return null;
     }
 
-
-    public void assignPackage(DeliveryPackage deliveryPackage) {
-        deliveryPackages.add(deliveryPackage);
-    }
-
     @Override
     public String print() {
         return String.format("Delivery route%n" +
-                            "Start location: %s%n",
-                            "End location: %s"); // TO BE IMPLEMENTED
+                        "Start location: %s%n",
+                "End location: %s"); // TO BE IMPLEMENTED
     }
 }
