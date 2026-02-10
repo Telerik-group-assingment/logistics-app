9b3d69d05ea9a90ae86f143e65e0fc543cfe089c
Spasimira Genova <spassimiragenova@gmail.com>
2026-02-10 21:29:42 +0200

    "ValidateStringLength method in utils"

--- logistics-app/src/logisticsapp/utils/ValidationHelpers.java
+++ logistics-app/src/logisticsapp/utils/ValidationHelpers.java
@@ -12,6 +12,12 @@ public class ValidationHelpers {
         }
     }
 
+    public static void validateStringLength(String stringToValidate, int minLength, int maxLength, String errorMessage) {
+        if (stringToValidate.length() < minLength || stringToValidate.length() > maxLength) {
+            throw new IllegalArgumentException(errorMessage);
+        }
+    }
+
     public static <E> void validateArgumentsCount(List<E> parameters,int expectedArgumentsCount){
         if(parameters.size() != expectedArgumentsCount){
             throw new IllegalArgumentException(String.format(INVALID_NUMBER_OF_ARGUMENTS,expectedArgumentsCount,parameters.size()));
